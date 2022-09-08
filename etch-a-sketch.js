59e94ef082e6932f67552ee280f41004ae368d1c
Oscar Elmahdy <opelm1@gmail.com>
2022-09-08 15:39:12 +0800

    Add and change to random color functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -51,7 +51,9 @@ function drawCanvas(n) {
   const squares = document.querySelectorAll(".square");
 
   squares.forEach((square) => {
-    square.addEventListener("mouseover", makeBlack);
+    // square.addEventListener("mouseover", makeBlack);
+    square.addEventListener("mouseover", makeRandColor);
+    // square.addEventListener("mouseover", progressiveBlack);
   });
 }
 
@@ -59,22 +61,22 @@ function makeBlack(e) {
   e.target.setAttribute("style", "background: black");
 }
 
-// Add a button to the top of the screen that will send the user a popup asking for the number of
-// squares per side for the new grid. Once entered, the existing grid should be removed and a new
-//grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got
-// a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of
-// squares results in more computer resources being used, potentially causing delays, freezing, or
-// crashing that we want to prevent.
-
-// - Research button tags in HTML and how you can make a JavaScript
-// function run when one is clicked.
-
-// - You should be able to enter 64 and have a brand new 64x64 grid pop up
-// without changing the total amount of pixels used.
-
 // Extra Credit
 // - Instead of just changing the color of a square from black to white
 // (for example), have each pass through with the mouse change it to a
 // completely random RGB value. Then try having each pass just add another
 // 10% of black to it so that only after 10 passes is the square
 // completely black.
+
+function makeRandColor(e) {
+  e.target.setAttribute("style", `background: ${randomRGB()}`);
+}
+
+function randomRGB() {
+  let r = Math.ceil(Math.random() * 255);
+  let g = Math.ceil(Math.random() * 255);
+  let b = Math.ceil(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function progressiveBlack() {}
